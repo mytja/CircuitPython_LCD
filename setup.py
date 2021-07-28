b1f05592c31a0455df3f212af70f83b44a83d136
mytja <mytja@protonmail.com>
2021-07-28 15:11:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "lcd"},
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
 )
