269badae67ce00cfd70ec1117dfb9017f0952968
mytja <mytja@protonmail.com>
2021-07-28 15:14:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,55 @@
-import setuptools
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
 
-with open("README.rst", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
 
-setuptools.setup(
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
     name="lcd",
-    version="0.0.1",
-    author="mytja",
-    author_email="mytja@protonmail.com",
-    description="A small example package",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    description="CircuitPython library for standard character LCDs.",
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
-    },
+    long_description_content_type="text/x-rst",
+    # The project's main homepage.
+    url="https://github.com/adafruit/Adafruit_CircuitPython_CharLCD",
+    # Author details
+    author="Adafruit Industries",
+    author_email="circuitpython@adafruit.com",
+    install_requires=[],
+    # Choose your license
+    license="MIT",
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Hardware",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
-    packages=setuptools.find_packages(),
-    python_requires=">=3.6",
+    # What does your project relate to?
+    keywords="adafruit character lcd rgb 16x2 20x4 hardware micropython circuitpython",
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=["lcd"],
 )
