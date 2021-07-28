f524205ebe02e2b0798ce51c336c5b8444e08be4
mytja <mytja@protonmail.com>
2021-07-28 15:11:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     package_dir={"": "lcd"},
-    packages=setuptools.find_packages(where="lcd"),
+    packages=setuptools.find_packages(),
     python_requires=">=3.6",
 )
